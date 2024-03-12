6f27c78ba3d598281bf289297d8129af6a02823b
YennyTR <Yenny.Yurani.Taborda@ibm.com>
2024-03-11 21:53:05 -0500

    initial

---
+++ src/main/java/com/appoiments/LaboratoryApoimentsApp.java
@@ -0,0 +1,13 @@
+package com.appoiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratoryApoimentsApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboratoryApoimentsApp.class, args);
+	}
+
+}
